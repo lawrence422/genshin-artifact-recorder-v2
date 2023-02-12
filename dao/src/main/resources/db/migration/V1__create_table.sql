d617d8eec6f396b481ff3230399a04980a35db35
lawrence422 <lawrence422@gmail.com>
2023-02-12 14:36:58 +0800

    feat: fix docker-compose v3

--- dao/src/main/resources/db/migration/V1__create_table.sql
+++ dao/src/main/resources/db/migration/V1__create_table.sql
@@ -14,24 +14,24 @@ CREATE TABLE IF NOT EXISTS `artifact_set_type_enum`(
     `artifact_set_type_abbrevation` varchar(45) NOT NULL,
     `artifact_set_type_name` varchar(45) NOT NULL,
     PRIMARY KEY(`artifact_set_type_abbrevation`)
-)
+);
 
 
-CREATE IF NOT EXISTS TABLE `artifact`
+CREATE TABLE IF NOT EXISTS  `artifact`
 (
-    `artifact_id`                        int NOT NULL AUTO_INCREMENT,
+    `artifact_id`                        INT NOT NULL AUTO_INCREMENT,
     `artifact_date`                      timestamp NULL DEFAULT CURRENT_TIMESTAMP,
     `artifact_set`                       varchar(45)  DEFAULT NULL,
     `artifact_type`                      varchar(45)  DEFAULT NULL,
     `artifact_main_stats`                varchar(45)  DEFAULT NULL,
     `artifact_sub_initial_stats_1`       varchar(45)  DEFAULT NULL,
-    `artifact_sub_initial_stats_1_value` double       DEFAULT NULL,
+    `artifact_sub_initial_stats_1_value` DOUBLE       DEFAULT NULL,
     `artifact_sub_initial_stats_2`       varchar(45)  DEFAULT NULL,
-    `artifact_sub_initial_stats_2_value` double       DEFAULT NULL,
+    `artifact_sub_initial_stats_2_value` DOUBLE        DEFAULT NULL,
     `artifact_sub_initial_stats_3`       varchar(45)  DEFAULT NULL,
-    `artifact_sub_initial_stats_3_value` double       DEFAULT NULL,
+    `artifact_sub_initial_stats_3_value` DOUBLE        DEFAULT NULL,
     `artifact_sub_initial_stats_4`       varchar(45)  DEFAULT NULL,
-    `artifact_sub_initial_stats_4_value` double       DEFAULT NULL,
+    `artifact_sub_initial_stats_4_value` DOUBLE        DEFAULT NULL,
     PRIMARY KEY (`artifact_id`),
     CONSTRAINT `artifact_set_fk` FOREIGN KEY(`artifact_set`) REFERENCES `artifact_set_type_enum` (`artifact_set_type_abbrevation`),
     CONSTRAINT `artifact_type_fk` FOREIGN KEY(`artifact_type`) REFERENCES `artifact_type_enum`(`artifact_type_abbrevation`)
